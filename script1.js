e543db8da9893d8c2211d50900f87b2851afccc6
akauzinha47 <kau.vmenezes@gmail.com>
2024-07-04 11:46:47 -0300

    feat: js

---
+++ script1.js
@@ -0,0 +1,49 @@
+import {service} from "./js/datebaseConfig.js"
+
+const endPoint = "/iotgato/site"
+
+// Definindo estrutura do corpo do meu objeto do banco.
+var body = {
+    
+}
+
+const loadData = () => {
+    // const d = new Date();
+
+    service.load(endPoint).then( data => {
+        body = data;
+        console.log(body);
+
+        setNames();
+
+    })
+
+}
+
+const setNames = () => {
+    const gato1 = document.getElementById("gato-1-nome");
+    const gato2 = document.getElementById("gato-2-nome");
+    const gato3 = document.getElementById("gato-3-nome");
+
+    gato1.innerHTML = body[0].Nome; 
+    gato2.innerHTML = body[1].Nome;
+    gato3.innerHTML = body[2].Nome;
+
+}
+
+const changePermission = (id) => {
+
+    let checkbox = document.querySelector('#id:checked')
+    console.log(id);
+
+}
+
+window.changePermission = changePermission
+
+setInterval(() => {
+    loadData();
+}, 2000);
+
+
+
+
